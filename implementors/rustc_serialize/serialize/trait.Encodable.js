717c60023a756159f650dfd5bbeaaecf2b508b57
Travis CI User <travis@example.org>
2018-01-03 18:05:21 +0000

    Documentation for rust-lang-deprecated/time@b117485

---
+++ implementors/rustc_serialize/serialize/trait.Encodable.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["time"] = ["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Encodable.html\" title=\"trait rustc_serialize::serialize::Encodable\">Encodable</a> for <a class=\"struct\" href=\"time/struct.Timespec.html\" title=\"struct time::Timespec\">Timespec</a>","impl <a class=\"trait\" href=\"https://doc.rust-lang.org/rustc-serialize/rustc_serialize/serialize/trait.Encodable.html\" title=\"trait rustc_serialize::serialize::Encodable\">Encodable</a> for <a class=\"struct\" href=\"time/struct.Tm.html\" title=\"struct time::Tm\">Tm</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
